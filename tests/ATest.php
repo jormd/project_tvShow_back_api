8c93f10f920bb392ab88575d2319cb783e296fa2
jormd <johann.romand@etudiant.univ-reims.fr>
2019-01-30 14:55:38 +0100

    initalidation de travis

---
+++ tests/ATest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romandjohann
+ * Date: 2019-01-30
+ * Time: 14:52
+ */
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ATest extends TestCase
+{
+
+    /**
+     *
+     */
+    public function testA()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
