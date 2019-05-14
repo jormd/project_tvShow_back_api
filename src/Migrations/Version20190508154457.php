591ba01119df17053edb1633da4d190befe31877
jormd <johann.romand@etudiant.univ-reims.fr>
2019-05-14 21:19:44 +0200

    correction migration

--- src/Migrations/Version20190508154457.php
+++ src/Migrations/Version20190508154457.php
@@ -22,7 +22,6 @@ final class Version20190508154457 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE genre (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tv_show_genre (tv_show_id INT NOT NULL, genre_id INT NOT NULL, INDEX IDX_376362345E3A35BB (tv_show_id), INDEX IDX_376362344296D31F (genre_id), PRIMARY KEY(tv_show_id, genre_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE tv_show_genre ADD CONSTRAINT FK_376362345E3A35BB FOREIGN KEY (tv_show_id) REFERENCES tv_show (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE tv_show_genre ADD CONSTRAINT FK_376362344296D31F FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE');
@@ -34,7 +33,6 @@ final class Version20190508154457 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE tv_show_genre DROP FOREIGN KEY FK_376362344296D31F');
-        $this->addSql('DROP TABLE genre');
         $this->addSql('DROP TABLE tv_show_genre');
     }
 }
